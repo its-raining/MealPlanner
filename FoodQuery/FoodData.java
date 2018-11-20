ff4c10718c8aa3d163f0e86262d2de8afb30b849
hnkoenig <43788239+hnkoenig@users.noreply.github.com>
2018-11-19 21:30:48 -0600

    Update FoodData.java

--- FoodQuery/FoodData.java
+++ FoodQuery/FoodData.java
@@ -1,6 +1,11 @@
+package application;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import javafx.collections.FXCollections;
 
 /**
  * This class represents the backend for managing all 
@@ -32,8 +37,30 @@ public class FoodData implements FoodDataADT<FoodItem> {
      */
     @Override
     public void loadFoodItems(String filePath) {
-        // TODO : Complete
-    	
+        foodItemList = FXCollections.observableArrayList();
+        try {
+    	File inFile = new File(filePath);
+    	Scanner readFile = new Scanner(inFile);
+    	int lineNum = 0;//Index to modify in foodItemList
+    	while (readFile.hasNextLine()) {
+    	    String[] currLine = readFile.nextLine().split(",");
+    	    if(currLine[0].equals("")) {
+    	        break;//Exits loop at end of data
+    	    }
+    	    foodItemList.add(new FoodItem(currLine[0], currLine[1]));//Sets ID and name
+    	    for (int i = 2; i < currLine.length; i+= 2) {//Adds Nutrients by name/value pairs
+    	        foodItemList.get(lineNum).addNutrient(currLine[i], Double.parseDouble(currLine[i+1]));
+    	        if (i + 1 == currLine.length - 2) {
+    	            break;//breaks loops on improper files
+    	        }
+    	    }
+    	   lineNum++; 
+    	} 	
+    	readFile.close();
+        }
+        catch(FileNotFoundException e) {
+            
+        }    
     }
 
     /*
@@ -81,5 +108,4 @@ public class FoodData implements FoodDataADT<FoodItem> {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
