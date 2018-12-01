35e2d5cb891404e27383de3adea7836095da9594
its-raining <43768723+its-raining@users.noreply.github.com>
2018-11-30 20:44:25 -0600

    Update MealAnalysisPane.java

--- FoodQuery/MealAnalysisPane.java
+++ FoodQuery/MealAnalysisPane.java
@@ -11,64 +11,87 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MealAnalysisPane {
-	private Button okayButton;
 	private HashMap<String, Double> mealAnalysis;
 	private ArrayList<Label> labelNames;
 	
-	public void start() {
+	public void start(MealData mealData) {
+		
+		//setup a new stage for meal analysis
         Stage stage = new Stage();
         stage.setTitle("Meal Analysis");
         
-        GridPane gridPane = new GridPane();
-        gridPane.setAlignment(Pos.CENTER);
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
-        gridPane.setPadding(new Insets(25, 25, 25, 25));
- 
-        addNutrientLabels();
+        BorderPane borderPane = new BorderPane();
         
-        for(int i = 0; i < labelNames.size(); i++) {
-        	 gridPane.add(labelNames.get(i), 0, i);
-        }
+        //use left-pane for numerical sum of each nutrients, center-pane for bar chart
+        borderPane.setLeft(addNutrientLabels(mealData));
+        borderPane.setCenter(analysis());               
         
-        okayButton = new Button("OK");
-        
-        handleOK(okayButton, stage);
-                
-        gridPane.add(okayButton, 1, 7);
-        
-        Scene scene = new Scene(gridPane,400,300);
+        Scene scene = new Scene(borderPane,400,300);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
 	}
 
-	private void handleOK(Button okayButton, Stage stage) {
-		okayButton.setOnAction(new EventHandler<ActionEvent>(){
-			@Override
-			public void handle(ActionEvent e) {
-				stage.close();
-			}
-		});	
-	}
-
-	private void addNutrientLabels() {
+	private VBox addNutrientLabels(MealData mealData) {
+		VBox vbox = new VBox();
+		
 		labelNames = new ArrayList<Label>();
+		mealAnalysis = mealData.getAnalysis();
+		
+		vbox.setAlignment(Pos.CENTER);
+		vbox.setSpacing(30);
+		vbox.setPadding(new Insets(10,10,10,10));
 		
 		labelNames.add(new Label("Calories:" + mealAnalysis.get("calories")));
 		labelNames.add(new Label("Fat:" + mealAnalysis.get("fat")));
-		labelNames.add(new Label("Carbs:" + mealAnalysis.get("carbonhydrate")));
+		labelNames.add(new Label("Carbs:" + mealAnalysis.get("carbohydrate")));
 		labelNames.add(new Label("Fiber:" + mealAnalysis.get("fiber")));
 		labelNames.add(new Label("Protein:" + mealAnalysis.get("protein")));
 		
+		vbox.getChildren().addAll(labelNames);
+		return vbox;
 	}
-
+	
+	private BarChart<String, Number> analysis() {
+		//x and y axis for bar chart
+	    final CategoryAxis xAxisNutrients = new CategoryAxis();
+        final NumberAxis yAxisGrams = new NumberAxis();
+        
+        //initialize a bar chart
+		final BarChart<String, Number> bc = new BarChart<String, Number>(xAxisNutrients, yAxisGrams);
+		bc.setTitle("Meal Analysis");
+		xAxisNutrients.setLabel("Nutrients");
+		yAxisGrams.setLabel("Grams");
+		
+		//initialize one series and add nutrients to it
+		XYChart.Series<String, Number> data = new XYChart.Series<>();
+		data.getData().add(new XYChart.Data<String, Number>("Fat", mealAnalysis.get("fat")));
+		data.getData().add(new XYChart.Data<String, Number>("Carbs", mealAnalysis.get("carbohydrate")));
+		data.getData().add(new XYChart.Data<String, Number>("Fiber", mealAnalysis.get("fiber")));
+		data.getData().add(new XYChart.Data<String, Number>("Protein", mealAnalysis.get("protein")));
+		
+		bc.getData().add(data);
+		
+		//hide series icon
+		bc.setLegendVisible(false);
+//		bc.setMaxWidth(150);
+//		bc.setMaxHeight(100);
+		
+		return bc;		
+	}
 
 }
