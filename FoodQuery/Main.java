a24776a48f52bbed709f233055823ce8621b9272
its-raining <43768723+its-raining@users.noreply.github.com>
2018-12-10 15:32:13 -0600

    Add files via upload

--- FoodQuery/Main.java
+++ FoodQuery/Main.java
@@ -14,47 +14,52 @@
  * Bugs:       N/A
  */
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 /**
- * This class contains the main method and setup the primary stage of mealplanner
+ * This class contains the main method and sets up the primary stage of Meal
+ * Planner
  * 
  * @author Aaron Hernandez
  * @author Xiao Fei
  * @author Henry Koenig
  */
 public class Main extends Application {
-	
+
 	/**
 	 * The start method that sets up the primary stage and scene for GUI
+	 * 
 	 * @param primaryStage
+	 *            The main stage (i.e. window) for the GUI to use
 	 */
 	@Override
 	public void start(Stage primaryStage) {
-		
+
 		try {
-			
+
 			BorderPane root = new BorderPane();
 			root = GUI.setupGUI(root);
-			Scene scene = new Scene(root,800,600);
+			Scene scene = new Scene(root, 800, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Meal Planner");
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
 			primaryStage.show();
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
-	 * The main method
+	 * The main method to launch the application
+	 * 
 	 * @param args
+	 *            The arguments passed into the Meal Planner GUI (unused)
 	 */
 	public static void main(String[] args) {
 		launch(args);
