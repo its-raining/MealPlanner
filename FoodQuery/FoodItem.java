8dd6809d841fe43b09ee2b34eb56b2f985b8f9ad
hnkoenig <43788239+hnkoenig@users.noreply.github.com>
2018-12-10 15:13:19 -0600

    Update FoodItem.java

--- FoodQuery/FoodItem.java
+++ FoodQuery/FoodItem.java
@@ -1,11 +1,19 @@
+/**
+ * Filename:   FoodItem.java
+ * Project:    Meal Planner
+ * Authors:    Henry Koenig, Xiao Fei, Aaron Hernandez
+ * 
+ * Additional credits: Java 8 API
+ *
+ * Bugs or other notes: None known.
+ * 
+ */
 package application;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 /**
- * This class represents a food item with all its properties.
+ * This class represents a food item and all its properties.
  * 
  * @author Aaron Hernandez
  * @author Xiao Fei
@@ -18,16 +26,14 @@ public class FoodItem {
 	// The id of the food item.
 	private String id;
 
-	// Map of nutrients and value.
+	// Map of nutrients and corresponding value.
 	private HashMap<String, Double> nutrients;
 
 	/**
-	 * Constructor
+	 * Constructor for FoodItem
 	 * 
-	 * @param name
-	 *            name of the food item
-	 * @param id
-	 *            unique id of the food item
+	 * @param name name of the food item
+	 * @param idunique id of the food item
 	 */
 	public FoodItem(String id, String name) {
 		this.id = id;
@@ -36,43 +42,49 @@ public class FoodItem {
 	}
 
 	/**
-	 * Gets the name of the food item
+	 * Gets the name of this food item
 	 * 
-	 * @return name of the food item
+	 * @return name of this food item
 	 */
 	public String getName() {
 		return this.name;
 	}
 
 	/**
-	 * Gets the unique id of the food item
+	 * Gets the unique id of this food item
 	 * 
-	 * @return id of the food item
+	 * @return the id field of this food item
 	 */
 	public String getID() {
 		return this.id;
 	}
 
 	/**
-	 * Gets the nutrients of the food item
+	 * Gets the nutrients of ths food item
 	 * 
-	 * @return nutrients of the food item
+	 * @return nutrients hash map of this food item
 	 */
 	public HashMap<String, Double> getNutrients() {
 		return this.nutrients;
 	}
 
 	/**
-	 * Adds a nutrient and its value to this food. If nutrient already exists,
-	 * updates its value.
+	 * Adds a nutrient and its value to this food. If nutrient already exists, update the value
+	 * associated with it.
+	 * 
+	 * @param name the name of of the nutrient to add
+	 * @param value the value of the nutrient associated with name
 	 */
 	public void addNutrient(String name, double value) {
 		this.nutrients.put(name.toLowerCase(), value);
 	}
 
 	/**
-	 * Returns the value of the given nutrient for this food item. If not present,
+	 * Returns the value associated with the given nutrient for this food item. If not present,
 	 * then returns 0.
+	 * 
+	 * @param name the name of the nutrient to find the value of
+	 * @return the value associated with the given nutrient name, 0 if name is not present
 	 */
 	public double getNutrientValue(String name) {
 		
